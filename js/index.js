e3e923b8f7a2c4090b801cbae792691dc76e33cc
Vivian Dien <vivian@flux.io>
2016-08-18 19:32:44 -0700

    added column chart

--- js/index.js
+++ js/index.js
@@ -687,54 +687,95 @@ function calculateImpacts(quartzDB, materialsWithMasses) {   // materialsWithMas
   };
 
   return result;
+  // var = chartResult = [ {"Impact" : "GWP"}, {"Impact" : "PED"}, {"Impact" : "Cancer - Red"}, {"Impact" : "Cancer - Orange"}  ];
+  
+  // for (var i = 0; i < chartResult.length; i++){
+  //   for (var j = 1; j < result.length; j++) {
+    
+  //   }    
+  // }
+
 
 }; 
 
-function createChart() {
-  var chart = new CanvasJS.Chart("columnChart",
-    {
-      title:{
-      text: "Number of Students in Each Room"   
-      },
-      animationEnabled: true,
-      axisX:{
-        title: "Rooms"
-      },
-      axisY:{
-        title: "percentage"
-      },
-      data: [
-      {        
-        type: "stackedColumn100",
-        name: "Boys",
-        showInLegend: "true",
-        dataPoints: [
-        {  y: 40, label: "Cafeteria"},
-        {  y: 10, label: "Lounge" },
-        {  y: 72, label: "Games Room" },
-        {  y: 30, label: "Lecture Hall" },
-        {  y: 21, label: "Library"}                
-        ]
-      }, {        
-        type: "stackedColumn100",        
-        name: "Girls",
-        showInLegend: "true",
-        dataPoints: [
-        {  y: 20, label: "Cafeteria"},
-        {  y: 14, label: "Lounge" },
-        {  y: 40, label: "Games Room" },
-        {  y: 43, label: "Lecture Hall" },
-        {  y: 17, label: "Library"}                
-        ]
-      }
-      ]
+
+  function createChart() {
+    var svg = d3.select("svg"),
+        margin = {top: 10, right: 80, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .rangeRound([0, width])
+        .padding(0.1)
+        .align(0.1);
+
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var z = d3.scaleOrdinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    var stack = d3.stack()
+        .offset(d3.stackOffsetExpand);     
+
+    
+    d3.csv("../data.csv", type, function(error, data) {
+      if (error) throw error;
+
+      data.sort(function(a, b) { return b[data.columns[1]] / b.total - a[data.columns[1]] / a.total; });
+
+      x.domain(data.map(function(d) { return d.Impact; }));
+      z.domain(data.columns.slice(1));
+
+      var serie = g.selectAll(".serie")
+        .data(stack.keys(data.columns.slice(1))(data))
+        .enter().append("g")
+          .attr("class", "serie")
+          .attr("fill", function(d) { return z(d.key); });
+
+      serie.selectAll("rect")
+        .data(function(d) { return d; })
+        .enter().append("rect")
+          .attr("x", function(d) { return x(d.data.Impact); })
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+          .attr("width", x.bandwidth());
+
+      g.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      g.append("g")
+          .attr("class", "axis axis--y")
+          .call(d3.axisLeft(y).ticks(10, "%"));
+
+      var legend = serie.append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d) { var d = d[d.length - 1]; return "translate(" + (x(d.data.Impact) + x.bandwidth()) + "," + ((y(d[0]) + y(d[1])) / 2) + ")"; });
+
+      legend.append("line")
+          .attr("x1", -6)
+          .attr("x2", 6)
+          .attr("stroke", "#000");
+
+      legend.append("text")
+          .attr("x", 9)
+          .attr("dy", "0.35em")
+          .attr("fill", "#000")
+          .style("font", "10px sans-serif")
+          .text(function(d) { return d.key; });
     });
 
-    chart.render();
+    function type(d, i, columns) {
+      for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+      d.total = t;
+      return d;
+    }
+
   };
-
-
-
 
 /**
  * Start the application.
